2e14b2ab704dd031416e66f3924d3bfed8daf46e
ShenLang <shenchenbo@mockuai.com>
2020-09-03 20:35:06 +0800

    init

---
+++ common-aliases.plugin.zsh
@@ -0,0 +1,17 @@
+alias l='ls -laFh'     #size,show type,human readable
+
+alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
+
+alias grep='grep --color'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
+
+alias t='tail -f'
+
+alias dud='du -d 1 -h'
+alias duf='du -sh *'
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
